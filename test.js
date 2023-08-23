fdabc270a7870f7ef281950e170847a9d75485a0
Garrett Stallsmith <gwstallsmith@gmail.com>
2023-08-22 23:15:45 -0400

    Added triangle, unit triangle, arc, unit arc

--- test.js
+++ test.js
@@ -71,6 +71,32 @@ function trueX(relativeX, unitAdjust = 1) { return relativeX/unitAdjust; }
 // Adjusts for Canvas flipping the y coordinates
 function trueY(relativeY, unitAdjust = 1) { return -relativeY/unitAdjust; }
 
+function quadArcTan(x, y) {
+    let angle;
+    if(x > 0) {
+        angle = Math.atan(y/x);
+    } else if (x < 0 && y >= 0) {
+        angle = Math.atan(y/x) + PI;
+    } else if(x < 0 && y < 0) {
+        angle = Math.atan(y/x) - PI;
+    } else if (x == 0 && y > 0) {
+        angle = PI/2;
+    } else if (x == 0 && y < 0) {
+        angle = -1 * PI/2;
+    } else if (x == 0 && y == 0) {
+        angle = NaN;
+    }
+
+    return angle;
+}
+
+function originDist() {
+    return (Math.sqrt(Math.pow(relativeMouseX(), 2) + (Math.pow(relativeMouseY(), 2))));
+}
+
+function mouseInCircle() {
+    return originDist <= UNIT;
+}
 
 
 function increaseCanvasSize() {
@@ -99,18 +125,23 @@ function decreaseCanvasSize() {
     setup();
 }
 
+let fontInconsolata;
 // P5 function.
 let canvas;
 function setup() {
     canvas = createCanvas(CANVAS_SIZE, CANVAS_SIZE, WEBGL);
     canvas.parent('p5');
 
-
+    textFont(fontInconsolata);
+    
+    textAlign(CENTER);
     textSize(24);
 }
 
 // P5 function.
 function preload() {
+    fontInconsolata = loadFont('Inconsolata.otf');
+
 }
 
 // P5 function.
@@ -120,9 +151,6 @@ function keyPressed() {
     }
     if(keyCode === LEFT_ARROW) {
         decreaseCanvasSize();
-    }
-    if(keyCode === UP_ARROW) {
-        zoomOnMouse();
     }
 
 }
@@ -140,7 +168,6 @@ function draw() {
 
     CosMan.strokeWeight(2);
 
-
     drawCircle();
 
     drawXAxis();
@@ -155,14 +182,20 @@ function draw() {
     //drawQuadIV();
 
     drawGrid();
+
+    drawArc();
+    //drawUnitArc();
+
+    drawTriangle()
+    drawUnitTriangle();
+
     drawSin();
     drawCos();
-    drawTan();
+    //drawTan();
 
-    drawCsc();
-    drawSec();
+    //drawCsc();
+    //drawSec();
     //drawCot();
-
     drawMousePoint();
 
 }
@@ -185,7 +218,7 @@ function drawYAxis() {
     line(0, -CANVAS_SIZE, 0, CANVAS_SIZE); // Y-AXIS
 }
 
-function drawCircle(diameter = 2 * UNIT) {
+function drawCircle(diameter = UNIT * 2) {
     CosMan.strokeColor('white');
     CosMan.strokeWeight(4);
     fill('#101111');
@@ -271,6 +304,72 @@ function drawGrid(gridDensity = 1) {
 
 }
 
+// TRIANGLES
+function drawTriangle() {
+    CosMan.strokeColor('white');
+    CosMan.strokeWeight(1);
+    fill('rgba(255,180,180, 0.5)');
+    triangle(0, 0, relativeMouseX(), 0, relativeMouseX(), relativeMouseY());
+    CosMan.strokeColor('red');
+    CosMan.strokeWeight(12);
+}
+
+function drawUnitTriangle() {
+    CosMan.strokeColor('white');
+    CosMan.strokeWeight(1);
+
+    fill('rgba(255,80,80, 0.5)');
+
+
+    let angle = quadArcTan(relativeMouseX(), -relativeMouseY()) / PI;
+    if(angle < 0) {
+        angle = quadArcTan(relativeMouseX(), -relativeMouseY()) / PI;
+        angle += 2;
+    }
+    angle *= PI;
+
+    if(mouseInCircle()) {
+        triangle(0, 0, relativeMouseX(), 0, relativeMouseX(), relativeMouseY());
+    } else {
+        triangle(0, 0, relativeCos(angle), 0, relativeCos(angle), relativeSin(angle));
+
+
+    }
+}
+
+// ARCS
+
+function drawArc() {
+    CosMan.strokeColor('white');
+    CosMan.strokeWeight(2);
+    fill('rgba(0, 255, 0, .5)');
+
+
+    let angle = quadArcTan(relativeMouseX(), relativeMouseY()) / PI;
+    if(angle < 0) {
+        angle = quadArcTan(relativeMouseX(), relativeMouseY()) / PI;
+        angle += 2;
+    }
+    angle *= PI;
+
+    arc(0, 0, UNIT * 2, UNIT * 2, angle, 0, PIE);
+}
+
+function drawUnitArc() {
+    CosMan.strokeColor('white');
+    CosMan.strokeWeight(2);
+    fill('rgba(0, 0, 255, .5)');
+
+    let angle = quadArcTan(relativeMouseX(), relativeMouseY()) / PI;
+    if(angle < 0) {
+        angle = quadArcTan(relativeMouseX(), relativeMouseY()) / PI;
+        angle += 2;
+    }
+    angle *= PI;
+
+    arc(0, 0, originDist() * 2, originDist() * 2, angle, 0, PIE)
+}
+
 // TRIG FUNCTIONS
 function drawSin() {
     CosMan.strokeColor('cyan');
@@ -295,23 +394,20 @@ function drawCos() {
 function drawTan() {
     CosMan.strokeColor('magenta');
     CosMan.strokeWeight(4);
-
-    line(UNIT, 0, UNIT, relativeMouseX()/relativeMouseY() * UNIT)
+    line(UNIT, 0, UNIT, (relativeMouseX()/relativeMouseY() * UNIT))
 }
 
 function drawCsc() {
     CosMan.strokeColor('lime');
     CosMan.strokeWeight(4);
-    let csc  = 1/relativeMouseY() * UNIT * 4;
-    console.log(csc);
-    line(0, 0, 0, csc);
+    line(0, 0, 0, UNIT/(relativeMouseY()/UNIT));
 
 }
 
 function drawSec() {
     CosMan.strokeColor('orange');
     CosMan.strokeWeight(4);
-    line(0, 0, UNIT, relativeMouseX()/relativeMouseY() * UNIT)
+    line(0, 0, UNIT, (relativeMouseX()/relativeMouseY()) * UNIT)
 }
 
 function drawCot() {
